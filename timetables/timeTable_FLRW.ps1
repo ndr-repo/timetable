f3473914395a6f94a10362a86b978660ec91b0e8
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2025-03-15 07:34:40 -0400

    Update timeTable_FLRW.ps1

--- timetables/timeTable_FLRW.ps1
+++ timetables/timeTable_FLRW.ps1
@@ -1,2 +1,2 @@
 ﻿$workingDir = pwd
-Get-ChildItem -Path $workingdir -File -Recurse | Sort-Object -Property LastWriteTimeUtc,Directory,Length,Name| Format-Table -Property CreationTimeUtc,LastWriteTimeUtc,LastAccessTimeUtc,Length,Name,Directory -Wrap -AutoSize | more
+Get-ChildItem -Path $workingdir -File -Recurse | Sort-Object -Property LastWriteTimeUtc,Directory,Length,Name| Format-Table -Property CreationTimeUtc,LastAccessTimeUtc,LastWriteTimeUtc,Length,Name,Directory -Wrap -AutoSize | more
